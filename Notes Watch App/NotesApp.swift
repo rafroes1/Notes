06764c35edf5edbb3fb2f89073b68652763d631f
rafroes1 <rafafroes@outlook.com>
2023-03-16 11:53:33 -0300

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Rafael Carvalho on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
